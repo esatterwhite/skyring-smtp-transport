2d8b8e130366f44e3da5d3c1593678ab9f1afb2e
esatterwhite <esatterwhite@wi.rr.com>
2017-07-30 21:04:44 -0500

    Initial implementation (#1)
    The main transport must be configured. So the main export is a factory function. that returns the smtp transport handler. It can be configured directly, or by environment variables.

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# skyring-smtp-transport
+# skyring-smtp-transport [![Build Status](https://travis-ci.org/esatterwhite/skyring-smtp-transport.svg?branch=master)](https://travis-ci.org/esatterwhite/skyring-smtp-transport)
+
 SMTP Transport for node-skyring
+
+The smtp transport exposes a configurable transport function. As a result, it must be manually
+passed to a skyring server, and cannot be auto loaded. Environment variables can also be used
+to configure the transport instead of passing configuration in directly
+
+```js
+const Skyring = require('skyring')
+const SMTP = require('@skyring/smtp-transport')
+
+new Skyring({
+  seeds: ['localhost:3456']
+, node: {host: 'localhost', port: 3456}
+, transports: [ SMTP({ host: 'smtp.mail.com', port: 578 }) ]
+})
+```
+
+Available Environment Variables
+
+| Variable | Usabage | Example |
+-----------|---------|---------|
+SKYRING_SMTP_PORT | port of smtp server to connect to | 25
+SKYRING_SMTP_HOST | host name or ip of the smtp server | smtp.mailserver.com
+SKYRING_SMTP_AUTHMETHOD | auth method to login with | 'PLAIN'
+SKYRING_SMTP_USER | smtp login user name | 'username'
+SKYRING_SMTP_PASS | smtp login password | 'password'
+
+
+
---
+++ test/smtp.spec.js
@@ -0,0 +1,100 @@
+'use strict'
+
+const test = require('tap').test
+    , Skyring = require('skyring')
+    , supertest = require('supertest')
+    , SMTP = require('../lib/smtp')
+    , SMTPServer = require('smtp-server').SMTPServer
+    , simpleParser = require('mailparser').simpleParser;
+    ;
+
+
+function createMailServer(done) {
+  return new SMTPServer({
+    secure: false
+  , name: 'localhost'
+  , authOptional: true
+  , onData:(stream, session, cb) => {
+      simpleParser(stream, (err, mail) => {
+        cb();
+        done(err, mail);
+      });
+    }
+  , onAuth:(auth, session, cb) => {
+      setImmediate(cb, null, {user:'abc'});
+    }
+  });
+}
+
+test('SMTP Transport valid', (t) => {
+  const state = {
+    smtp_server: null
+  , skyring: null
+  }
+
+
+  t.test('setup skyring', (tt) => {
+    state.skyring = new Skyring({
+      seeds: ['localhost:3456']
+    , transports:[SMTP({
+        port:2222
+      , host: 'localhost'
+      })]
+    , node: {
+        port: 3456
+      , host: 'localhost'
+      , app: 'smtp'
+      }
+    }).load().listen(3000, null, null, tt.end);
+  });
+
+  t.test('set smtp timeout', (tt) => {
+    tt.plan(5)
+    const smtp = createMailServer((err, mail) => {
+      tt.equal(mail.text.trim(), 'hello world', 'text')
+      tt.equal(mail.to.text, 'eric@codedependant.net', 'to')
+      tt.equal(mail.from.text, 'mail@mail.com', 'from')
+    });
+    tt.on('end', () => {
+      smtp.close();
+    });
+    tt.test('smtp listen', (ttt) => {
+      smtp.listen(2222, ttt.end);
+    });
+
+
+    tt.test('set timeout', (ttt) => {
+      supertest('http://localhost:3000')
+        .post('/timer')
+        .set({
+          'Content-Type': 'application/json'
+        })
+        .send({
+          timeout: 500
+        , data: JSON.stringify({
+            text: 'hello world'
+          , to: 'eric@codedependant.net'
+          , from : 'mail@mail.com'
+          })
+        , callback: {
+            transport: 'smtp'
+          , uri: ''
+          , method: ''
+          }
+        })
+        .expect(201)
+        .end((err, res) => {
+          ttt.error(err);
+          ttt.end();
+        });
+    });
+  });
+
+  t.test('teardown', (tt) => {
+    state.skyring.close(() => {
+      tt.comment('skyring shut down')
+      tt.end()
+    })
+  });
+  t.end();
+});
